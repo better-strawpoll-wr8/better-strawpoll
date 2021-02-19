7f53657c542effbed90813e8a938a4a378df2b55
CameronDavis88 <cammien88@gmail.com>
2021-02-18 17:07:10 -0700

    fixed a few bugs

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,51 +1,64 @@
 import axios from 'axios'
 import React, { userState, useEffect, useState } from 'react'
-import { useSelector} from 'react-redux'
-import { getEndedPolls } from '../../../server/controllers/mainController'
 import Header from '../Header/Header'
-import {useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 //Styling Imports
 import './Dashboard.scss'
-import axios from 'axios'
-
 
 
 const Dashboard = (props) => {
-    const user = useSelector(state=> state.user)
-    // const dispatch = useDispatch()
-    // console.log('user:',user)
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    console.log('user:',user)
 
+    const [yourPolls, setYourPolls] = useState([])
     const [recentlyCreated, setRecentlyCreated] = useState([])
     const [recentlyEnded, setRecentlyEnded] = useState([])
-    
+
     const createNewPoll = () => {
         props.history.push('/create-poll')
     }
 
-    const getRecentPolls = () => {
+    const getYourPolls = () => {
         const id = user.user_id
+        axios.get('/api/polls/', { id })
+            .then(res => {
+                setYourPolls(res.data)
+            })
+            .catch(err => console.log(err))
+    }
+
+    const getRecentPolls = () => {
         axios.get('/api/recent_polls/')
-        .then(res => {
-            setRecentlyCreated(res.data)
-        })
-        .catch(err => console.log(err))
+            .then(res => {
+                setRecentlyCreated(res.data)
+            })
+            .catch(err => console.log(err))
     }
 
     const getEndedPolls = () => {
-        const id = user.user_id
+
         axios.get('/api/ended_polls/')
-        .then(res => {
-            setRecentlyEnded(res.data)
-        })
-        .catch(err => console.log(err))
+            .then(res => {
+                setRecentlyEnded(res.data)
+            })
+            .catch(err => console.log(err))
     }
 
-
-
     useEffect(() => {
+        getYourPolls()
         getRecentPolls()
         getEndedPolls()
     }, [])
+
+    const mappedYourPolls = yourPolls.map(poll => {
+        <div key={poll.poll_id}>
+            <span>{poll.subject}</span>
+            <span>{poll.options}</span>
+            <span>{poll.dateCreated}</span>
+            <span>{poll.expiryDate}</span>
+        </div>
+    })
 
     const mappedRecentPolls = recentlyCreated.map(poll => {
         <div key={poll.poll_id}>
@@ -64,30 +77,26 @@ const Dashboard = (props) => {
             <span>{poll.expiryDate}</span>
         </div>
     })
-   
-
-  
 
     return (
         <div className='dashboard'>
             <Header />
-            {/* <h1 className='your-polls-text'>Your Polls</h1> */}
-                <section>
-                    <h2 className='create-poll-tab' onClick={createNewPoll}>Create New Poll</h2>
-                    <div className='recently-created-polls-box'>
-                        <h2 className='polls-text' >Recently Created Polls</h2>
-                       {mappedRecentPolls}
-                       {mappedEndedPolls}
-                    </div>
-                    <div className='recently-ended-poll-box'>
-                        <h2 className='polls-text'>Recently Ended Polls</h2>
-                    </div>
-                </section>
+            <div className='your-polls-box'>
+                <h1 className='your-polls-text'>Your Polls</h1>
+                {mappedYourPolls}
+            </div>
+            <h2 className='create-poll-tab' onClick={createNewPoll}>Create New Poll</h2>
+            <div className='recently-created-polls-box'>
+                <h2 className='polls-text' >Recently Created Polls</h2>
+                {mappedRecentPolls}
+            </div>
+            <div className='recently-ended-poll-box'>
+                <h2 className='polls-text'>Recently Ended Polls</h2>
+                {mappedEndedPolls}
+            </div>
         </div>
 
     )
 }
-
-
 
 export default Dashboard;
