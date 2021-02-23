898c17c1ed0e880234e7345e6eabaf4e14c45b58
CameronDavis88 <cammien88@gmail.com>
2021-02-23 16:21:19 -0700

    blocks nonusers from some components and some styling onadashboard and createpost

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,12 +1,21 @@
 import React, {userState, useEffect, useState} from 'react'
+import Header from '../Header/Header'
 //Styling Imports
 import './Results.scss'
 
 const Results = (props) => {
 
 
+
+ 
+
+    useEffect(() => {
+    
+    }, [])
+
     return (
         <div className='results'>
+            <Header/>
             Results PAGE
         </div>
     )
