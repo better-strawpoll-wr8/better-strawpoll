90fa2f0f01eccc0dcd4f1ea54894c66c1147b0c9
Teddy-Hung <tedhungkk@gmail.com>
2021-02-23 13:13:09 -0800

    Added more flexbox options in scss mixins, added get_user_by_id module

--- src/Components/Poll/Poll.js
+++ src/Components/Poll/Poll.js
@@ -8,10 +8,20 @@ import axios from 'axios'
 
 const Poll = (props) => {
     const [poll, setPoll] = useState({})
-    const id = props.location.state.poll.poll_id
+    const [pollAuthor, setPollAuthor] = useState({})
+    const pollId = props.location.state.poll.poll_id
+    const authorId = props.location.state.poll.user_id
+    console.log('authorId: ',authorId)
 
     useEffect(() => {
-        axios.get(`/api/poll/${id}`)
+        axios.get(`/api/user/${authorId}`)
+            .then(res=> {
+                console.log(res.data)
+                setPollAuthor(res.data)
+            })
+            .catch(err => console.log(err))
+
+        axios.get(`/api/poll/${pollId}`)
             .then(res => {
                 console.log('data: ',res.data)
                 setPoll(res.data)
@@ -22,10 +32,24 @@ const Poll = (props) => {
     }, [])
 
     return (
-        <div className='poll'>
+        <>
             <Header />
-            Poll Page
-        </div>
+            <div className='poll'>
+                <section>
+                    <h2> {poll.subject}</h2>
+                    {/* {poll.options.optionsListTrim.map((e, i) => {
+                        return (
+                            <div key={i}>
+                                <span>{JSON.stringify(e.optionName).replace(/["]+/g, '')}: {JSON.stringify(e.voteCount).replace(/["]+/g, '')}</span>
+                            </div>)
+                    })} */}
+                </section>
+                <section>
+                    <h3>Poll Created By: {pollAuthor.username}</h3>
+                    <img className='poll-author-profile-img' src={pollAuthor.profile_picture}/>
+                </section>
+            </div>
+        </>
     )
 }
 
