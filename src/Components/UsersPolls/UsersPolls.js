7cf48faaa6507f48e0e304507358d796bbcefe15
CameronDavis88 <cammien88@gmail.com>
2021-03-01 14:07:35 -0700

    usersPoll is now displaying polls as it does in Dashboard

--- src/Components/UsersPolls/UsersPolls.js
+++ src/Components/UsersPolls/UsersPolls.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 import React, {userState, useEffect, useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import {updateUser} from '../../redux/reducer'
+// import {updateUser} from '../../redux/reducer'
+import { withRouter, Link } from 'react-router-dom'
+import { Pie } from 'react-chartjs-2';
 import Header from '../Header/Header'
 //Styling Imports
 import './UsersPolls.scss'
@@ -36,22 +38,44 @@ const getYourPolls = () => {
 
 
     const mappedYourPolls = yourPolls.map(poll => {
-        return(
-            <div key={poll.poll_id} className='mapped-poll'>
-            <h4> </h4>
-            <span className='tlte'> Title: {poll.subject}</span>
-            <h4> </h4>
-            <span className='date-created'>Date Created: {`${poll.date_created}`}</span>
-            <h4> </h4>
-            <span className='expiry-date'>Expiry Date: {`${poll.expiry_date}`}</span>
-            <h4> </h4>
-            <span className='options'>Options:{poll.options.optionsListTrim.map((e, i) => {
-                return (
-                    <div key={i}>
-                        <span>{JSON.stringify(e.optionName).replace(/["]+/g, '')}: {JSON.stringify(e.voteCount).replace(/["]+/g, '')}</span>
-                    </div>)
-            })}</span>
-        </div>)
+
+        const colors = poll.options?.optionsListTrim.map(() => '#' + Math.floor(Math.random() * 16777215).toString(16))
+
+        const data = {
+            labels: poll.options?.optionsListTrim.map(el => el.optionName),
+            datasets: [
+                {
+                    label: '# of votes',
+                    data: poll.options?.optionsListTrim.map(el => el.voteCount),
+                    backgroundColor: colors,
+                    borderColor: colors,
+                    borderWidth: 1
+                }
+            ]
+        }
+
+        const dateCreatedStr = JSON.stringify(poll.date_created).replace(/["]+/g, '')
+        const dateExpStr = JSON.stringify(poll.expiry_date).replace(/["]+/g, '')
+
+        const dateCreated = dateCreatedStr.slice(0, 10)
+        const timeCreated = dateCreatedStr.slice(11, 16)
+
+        const dateExp = dateExpStr.slice(0, 10)
+        const timeExp = dateExpStr.slice(11, 16)
+
+        return (
+            <div className='mapped-poll'>
+                <Link to={`/polls/${poll.poll_id}`} key={poll.poll_id}>
+                    <h4 className='tlte'> Title: {poll.subject}</h4>
+                </Link>
+                <h4 className='date-created'>Date Created: {dateCreated} Time: {timeCreated}</h4>
+                <h4 className='expiry-date'>Expires: {dateExp} Time: {timeExp}</h4>
+                <div className='results'>
+                    <Pie data={data} />
+                </div>
+            </div>
+        )
+
     })
 
     return (
